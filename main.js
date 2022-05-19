f7fae92fe278c497838ac9b442ae3210e696e610
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-05-19 14:32:49 -0400

    made another one

--- main.js
+++ main.js
@@ -14,3 +14,9 @@ console.log('#+#    #+#      #+#               #+#    #+#       #+#             
 console.log('#########       ##########        ###    ###       ##########       ###    ###      ##########        ###    ####       ########       ###   ###         ###');    
 
 
+console.log("B)bbbb              H)    hh                            N)n   nn                     !)  ");
+console.log("B)   bb             H)    hh                            N)nn  nn                    !)11 ");
+console.log("B)bbbb   e)EEEEE    H)hhhhhh e)EEEEE  r)RRR  e)EEEEE    N) nn nn  o)OOO  w)      WW !)11 ");
+console.log("B)   bb  e)EEEE     H)    hh e)EEEE  r)   RR e)EEEE     N)  nnnn o)   OO w)  WW  WW  !)  ");
+console.log("B)    bb e)         H)    hh e)      r)      e)         N)   nnn o)   OO w)  WW  WW      ");
+console.log("B)bbbbb   e)EEEE    H)    hh  e)EEEE r)       e)EEEE    N)    nn  o)OOO   w)WW WWW   !)  ");                                                        
